11c2110f004ede2cf3947b8932a3f26143fc0fd1
Hasta <tamanghasta@gmail.com>
2015-07-02 23:31:34 +0100

    inapp browser

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -6,13 +6,21 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.console": "0.2.13",
-    "org.apache.cordova.device": "0.3.0"
+    "org.apache.cordova.device": "0.3.0",
+    "org.apache.cordova.inappbrowser": "0.6.0"
 }
 // BOTTOM OF METADATA
 });
--- www/js/app.js
+++ www/js/app.js
@@ -1,31 +1,71 @@
 /**
- * Created by hasta on 01/07/15.
+ * Created by hasta on 02/07/15.
  */
-// We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
-function onLoad() {
-    console.log("On Load");
-    document.addEventListener("deviceready", onDeviceReady, false);
-    document.addEventListener("createIframe", createIframe, false);
-}
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        var ref = window.open('http://talk.build/?mobile=1', '_blank', 'location=no');
+        ref.addEventListener('loadstart', function(event) {
+//            alert('start: ' + event.url);
+            document.getElementsByClassName("loading").show();
 
-// Cordova is loaded and it is now safe to make calls Cordova methods
-//
-function onDeviceReady() {
-    document.addEventListener("createIframe", createIframe, false);
-}
+        });
+        ref.addEventListener('loadstop', function(event) {
+//            alert('stop: ' + event.url);
+            ref.insertCSS({file: "css/override.css"});
+        });
+        ref.addEventListener('loaderror', function(event) {
+            alert('error: ' + event.message);
+        });
+        ref.addEventListener('exit', function(event) {
+            alert("Exit from TalkBuild App?");
+        });
 
-// Handle the resume event
-//
-function onResume() {
-}
+        //app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-//doesn't block the load event
-function createIframe() {
-    var i = document.createElement("iframe");
-    i.src = "http://uat.talk.build/?mobile=1";
-    i.scrolling = "auto";
-    i.frameborder = "0";
-    i.width = "100%";
-    i.height = "100%";
-    document.getElementById("frameContainer").appendChild(i);
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
 };
+
+app.initialize();
