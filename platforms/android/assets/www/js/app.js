977a5dcdeb1ecd11e5b954b35aaebc5d99af84bb
Hasta <tamanghasta@gmail.com>
2015-07-05 17:44:05 +0100

    android sdk target ver 19

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -25,18 +25,12 @@ var app = {
         console.log("App Init");
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+   
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
            
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+  
     onDeviceReady: function () {       
         var networkState = checkConnection();
       
