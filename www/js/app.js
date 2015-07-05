33370faad8e495887d3cdda047714326f52b75c9
Hasta <tamanghasta@gmail.com>
2015-07-05 12:41:03 +0100

    DeviceReady not firing in device

--- www/js/app.js
+++ www/js/app.js
@@ -47,21 +47,6 @@ var app = {
             window.location = "http://talk.build";
         }
     },
-
-    //checkConnection: function () {
-    //    navigator.notification.alert('Checking connection');
-    //    var networkState = navigator.network.connection.type;
-    //    var states = {};
-    //    states[Connection.UNKNOWN] = 'Unknown connection';
-    //    states[Connection.ETHERNET] = 'Ethernet connection';
-    //    states[Connection.WIFI] = 'WiFi connection';
-    //    states[Connection.CELL_2G] = 'Cell 2G connection';
-    //    states[Connection.CELL_3G] = 'Cell 3G connection';
-    //    states[Connection.CELL_4G] = 'Cell 4G connection';
-    //    states[Connection.NONE] = 'No network connection';
-
-    //    return networkState;
-    //},
    
     // Update DOM on a Received Event
     receivedEvent: function(id) {
