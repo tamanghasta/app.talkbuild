e2ab5dc24060d37c05cb8f910e4d36f2b28c8c7c
Hasta <tamanghasta@gmail.com>
2015-07-05 18:06:37 +0100

    network plugin

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -36,6 +36,21 @@ module.exports = [
             "cordova.InAppBrowser.open",
             "window.open"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "id": "cordova-plugin-network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "id": "cordova-plugin-network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -45,7 +60,8 @@ module.exports.metadata =
     "org.apache.cordova.device": "0.3.0",
     "org.apache.cordova.dialogs": "0.3.0",
     "org.apache.cordova.vibration": "0.3.13",
-    "cordova-plugin-inappbrowser": "1.0.1"
+    "cordova-plugin-inappbrowser": "1.0.1",
+    "cordova-plugin-network-information": "1.0.1"
 }
 // BOTTOM OF METADATA
 });
